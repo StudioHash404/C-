a5d803080dbdf501db96342ac11c7f215ceaaad9
Memoryleak <182170131+StudioHash404@users.noreply.github.com>
2025-05-28 19:37:58 -0300

    Criar entradaSaida.cpp
    Aprendendo entrada e saída em C++

---
+++ entradaSaida.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num1, num2;
+
+	cout << "Digite um numero: " << endl;
+	cin >> num1;
+
+	cout << "Digite outro numero: " << endl;
+	cin >> num2;
+
+	cout << "Seus numeros digitados sao: " << num1 << " e " << num2 << endl;
+	return 0;
+}
